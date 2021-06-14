003e64b8f84bbe4012af558d12a53c156a02016f
Joe Witzke <joewitzke@joes-mbp.lan>
2021-06-14 16:13:03 -0600

    auth-router.js complete..still need to add/adjust auth-middleware

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,20 +1,45 @@
 // Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
 // middleware functions from `auth-middleware.js`. You will need them here!
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const User = require('../users/users-model');
 
-router.post('/register', (req, res, next) => {
-  res.json('registering new user');
-  next();
+router.post('/register', async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const hash = bcrypt.hashSync(password, 8);
+    const newUser = { username, password: hash };
+    const createdUser = await User.add(newUser);
+    res.json(createdUser);
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.post('/login', (req, res, next) => {
-  res.json('login  user');
-  next();
+router.post('/login', async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const [user] = await User.findBy({ username });
+    if (user && bcrypt.compareSync(password, user.password)) {
+      req.session.user = user;
+      res.json(`welcome ${user.username}!`);
+    } else {
+      next({ status: 401, message: 'invalid credentials' });
+    }
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.get('/logout', (req, res, next) => {
-  res.json('logout  user');
-  next();
+router.get('/logout', async (req, res, next) => {
+  if (req.session.user) {
+    req.session.destroy((err) => {
+      if (err) res.json({ message: 'no session' });
+      else res.json({ message: 'logged out' });
+    });
+  } else {
+    res.json({ message: 'no session' });
+  }
 });
 
 module.exports = router;
